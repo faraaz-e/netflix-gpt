397b144f2119f7044a802c3fb8d40d96b7a7ef59
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-04 21:40:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-red-600">
-      Hello Everyone, let's build Netflix GPT
-    </div>
+    <Body />
   );
 }
 
