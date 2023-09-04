0a07e16e914369b6f1606ad2a4627e793b80f270
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-04 21:04:37 +0530

    Project created & configured Tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-600">
+      Hello Everyone, let's build Netflix GPT
+    </div>
+  );
+}
+
+export default App;
