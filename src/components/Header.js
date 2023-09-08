c8c021da9baf79801e7bace94dfc36eb786cf3ed
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-08 20:05:28 +0530

    Unsubscribe from redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -42,6 +42,9 @@ const Header = () => {
         navigate("/");
       }
     });
+
+    // Unsubscribe when component unmounts
+    return () => unsubscribe();
   }, []);
 
   return (
