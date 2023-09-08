3571fd078f6155764068841593a53a69ffb6dd0a
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-08 20:17:44 +0530

    Adding constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR =
+  "https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg";
