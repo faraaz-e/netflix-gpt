b7615eb1269d9f4c2396d94dd2023c1744143e29
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-08 17:35:13 +0530

    Added Sign In / Sign Up
    Using Firebase API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,163 @@
-const Header = () => {
+import { useRef, useState } from "react";
+import Header from "./Header";
+import { validateData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    // Validate form data
+    const errorMessage = validateData(
+      isSignInForm ? name.current : name.current.value,
+      email.current.value,
+      password.current.value
+    ); // args received from useRef Hook
+    setError(errorMessage);
+
+    if (errorMessage) return;
+
+    if (!isSignInForm) {
+      // Sign Up
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL:
+              "https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setError(error.message);
+            });
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorCode + "-" + errorMessage);
+          // ..
+        });
+    } else {
+      // Sign in
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          navigate("/browse");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
-      <img
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-        className="w-44"
-      />
+    <div>
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="login_bg"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute p-12 bg-black top-20 left-1/3 bg-opacity-90 flex flex-col"
+      >
+        <h1 className="text-left font-bold text-white text-3xl pb-8">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full name"
+            className="p-3 m-3 w-80 rounded-lg bg-stone-800 text-white"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email or phone number"
+          className="p-3 m-3 w-80 rounded-lg bg-stone-800 text-white"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-3 m-3 w-80 rounded-lg bg-stone-800 text-white"
+        />
+
+        <p className="mx-3 py-2 text-sm text-amber-600">{error}</p>
+
+        <button
+          className="p-3 m-3 w-80 rounded-lg font-medium bg-red-600 text-white"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+
+        <p className="py-4 m-3 text-stone-500 m-3">
+          {isSignInForm ? "New to Netflix ? " : "Already a user ? "}
+          <span
+            className="hover:underline hover:cursor-pointer font-medium text-white"
+            onClick={toggleSignInForm}
+          >
+            {isSignInForm ? "Sign up now" : "Sign in now"}
+          </span>
+          .
+        </p>
+        <p className="py-4 m-3 text-stone-500 m-3 text-sm">
+          Disclaimer: This App is for educational purpose only.
+        </p>
+      </form>
     </div>
   );
 };
 
-export default Header;
+export default Login;
