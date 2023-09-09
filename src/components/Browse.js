5f8a3aae61b09bc4d2d2da5b12ef98c97a98aac7
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-09 14:30:21 +0530

    Added API and updated Slice

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch("https://api.tvmaze.com/shows?page=1");
+    const jsonData = await data.json();
+    console.log(jsonData);
+    dispatch(addNowPlayingMovies(jsonData));
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   return (
     <div>
       <Header />
