72f65d8b7b99a0627802a68d3ab1e94a9003bdf9
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-09 14:44:17 +0530

    Refactoring the code by adding Hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,22 +1,9 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch("https://api.tvmaze.com/shows?page=1");
-    const jsonData = await data.json();
-    console.log(jsonData);
-    dispatch(addNowPlayingMovies(jsonData));
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  
+  useNowPlayingMovies();
 
   return (
     <div>
