91597cdb1f1288d93538a36f402c5692ead2ff86
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-11 13:02:30 +0530

    Updated Video Background
    Fetches trailer link from Redux store

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
+
 const VideoBackground = () => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  useMovieTrailer();
+
   return (
     <>
-      <div>VideoBackground</div>
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src={"https://www.youtube.com/embed/" + trailerVideo}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
     </>
   );
 };
