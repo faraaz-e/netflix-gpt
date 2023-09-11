3e233d8368f6fa1c7b8eafd03618becd8ca6c694
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-11 12:58:04 +0530

    Created Video Title

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,17 @@
-const VideoTitle = () => {
+const VideoTitle = ({ title, overview }) => {
   return (
-    <>
-      <div>VideoTitle</div>
-    </>
+    <div className="pt-36 px-12">
+      <h1 className="text-4xl font-bold">{title}</h1>
+      <p className="py-6 w-1/2">{overview.replace(/(<([^>]+)>)/gi, "")}</p>
+      <div>
+        <button className="bg-white text-black px-4 py-2 w-32 rounded-sm">
+          ▶ Play
+        </button>
+        <button className="mx-2 bg-gray-500 text-white bg-opacity-50 px-4 py-2 w-32 rounded-sm">
+          🛈 More Info
+        </button>
+      </div>
+    </div>
   );
 };
 
