a1e40add3a9c923c930c4610650d822e65ddf66d
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-11 13:00:49 +0530

    Added movie trailer hook
    Fetches movie trailer link from API and adds to redux store

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useEffect, useState } from "react";
+
+const useMovieTrailer = () => {
+  const dispatch = useDispatch();
+  // const [trailerKey, setTrailerKey] = useState(null);
+
+  // Fetch video trailer link, if it is present in API
+  const getMovieVideos = async () => {
+    //   const data = await fetch("API_URL");
+    //   const jsonData = await data.json();
+    const trailer = "ct_w6Ne4-IQ";
+    // setTrailerKey(trailer);
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+};
+
+export default useMovieTrailer;
