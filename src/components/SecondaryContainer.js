c8d8990edf1e4622543c7b1131d56e09e8c89a3e
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-11 16:40:48 +0530

    Created Secondary Container
    Added Movie List and Movie Cards

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,8 +1,31 @@
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
 const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
   return (
-    <>
-      <div>SecondaryContainer</div>
-    </>
+    movies.nowPlayingMovies && (
+      <div className="bg-black pl-4">
+        <div className="-mt-40 relative z-10">
+          <MovieList
+            title={"Now Playing"}
+            movies={movies?.nowPlayingMovies.slice(0, 20)}
+          />
+        </div>
+        <MovieList
+          title={"Trending"}
+          movies={movies?.nowPlayingMovies.slice(20, 40)}
+        />
+        <MovieList
+          title={"Popular"}
+          movies={movies?.nowPlayingMovies.slice(40, 60)}
+        />
+        <MovieList
+          title={"Upcoming Movies"}
+          movies={movies?.nowPlayingMovies.slice(60, 80)}
+        />
+      </div>
+    )
   );
 };
 
