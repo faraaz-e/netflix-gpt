d402b0b8b6b80a29eb8a940867f33091d2440d0d
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-11 14:13:31 +0530

    Updated UI

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,10 +1,10 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
-      <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="py-6 w-1/2">{overview.replace(/(<([^>]+)>)/gi, "")}</p>
+    <div className="pt-[20%] px-12 absolute bg-gradient-to-r from-black w-screen aspect-video">
+      <h1 className="text-4xl font-bold text-white">{title}</h1>
+      <p className="py-6 w-1/4 text-white">{overview.replace(/(<([^>]+)>)/gi, "")}</p>
       <div>
-        <button className="bg-white text-black px-4 py-2 w-32 rounded-sm">
+        <button className="bg-white text-black px-4 py-2 w-32 rounded-sm hover:bg-opacity-80">
           ▶ Play
         </button>
         <button className="mx-2 bg-gray-500 text-white bg-opacity-50 px-4 py-2 w-32 rounded-sm">
