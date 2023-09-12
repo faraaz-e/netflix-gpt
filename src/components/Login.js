da8ef854b170f831617bf55ed3ca2bd3513a3f1b
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-12 15:06:16 +0530

    Updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { USER_AVATAR } from "../utils/constants";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -97,10 +97,7 @@ const Login = () => {
     <div>
       <Header />
       <div>
-        <img
-          src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-          alt="login_bg"
-        />
+        <img src={BG_URL} alt="login_bg" />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
