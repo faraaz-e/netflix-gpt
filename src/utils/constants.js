eb8f0970ed8754f49390cccc510d41012d2ffce6
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-12 16:21:07 +0530

    Added multi language

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,8 @@ export const USER_AVATAR =
 
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+];
