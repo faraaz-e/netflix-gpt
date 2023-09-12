30deecd42f7710674812330ba82dd847ab501b18
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-12 15:05:49 +0530

    Updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://wallpapers.com/images/hd/netflix-profile-pictures-5yup5hd2i60x7ew3.jpg";
+
+export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg";
