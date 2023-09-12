21a034749feead0c42072f5a68b1c416a1272ec8
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-12 17:01:29 +0530

    Added multi-language supportible

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,13 +1,20 @@
+import { useSelector } from "react-redux";
+import lang from "../utils/languageConstants";
+
 const GptSearchBar = () => {
+  const langKey = useSelector((store) => store.config.lang);
+
   return (
     <div className="bg-black h-screen">
       <form className="flex justify-center pt-28">
         <input
           type="text"
           className="p-2 mx-3 w-96 rounded-lg"
-          placeholder="What would you like to watch ?"
+          placeholder={lang[langKey].gptSearchPlaceholder}
         />
-        <button className="py-2 px-4 bg-red-600 text-white rounded-lg">Search</button>
+        <button className="py-2 px-4 bg-red-600 text-white rounded-lg">
+          {lang[langKey].search}
+        </button>
       </form>
     </div>
   );
