77ef65ca95072377c80aeef88a8bd2cd0f49ccdc
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-14 20:53:21 +0530

    UI responsive

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,8 +3,8 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ title, movies }) => {
   return (
     <div className="px-6">
-      <h1 className="text-2xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
+      <h1 className="text-xl md:text-2xl py-4 text-white">{title}</h1>
+      <div className="flex overflow-x-scroll no-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} image={movie?.image} />
