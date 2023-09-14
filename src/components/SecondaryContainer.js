c450a5aa0f9549f867670c7a53fbaa1d3e3a1719
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-14 20:52:26 +0530

    UI responsive

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,8 +5,8 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     movies.nowPlayingMovies && (
-      <div className="bg-black pl-4">
-        <div className="-mt-40 relative z-10">
+      <div className="bg-black pl-1 md:pl-4">
+        <div className="mt-0 md:-mt-40 relative z-10">
           <MovieList
             title={"Now Playing"}
             movies={movies?.nowPlayingMovies.slice(0, 20)}
@@ -23,6 +23,10 @@ const SecondaryContainer = () => {
         <MovieList
           title={"Upcoming Movies"}
           movies={movies?.nowPlayingMovies.slice(60, 80)}
+        />
+        <MovieList
+          title={"New Movies"}
+          movies={movies?.nowPlayingMovies.slice(80, 100)}
         />
       </div>
     )
