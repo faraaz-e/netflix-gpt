e782ce5244b0b557e1830882069f2ffd27962ab0
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-14 20:52:50 +0530

    UI responsive

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,7 @@ const MovieCard = ({ image }) => {
   if (!image) return null;
 
   return (
-    <div className="w-48 pr-4">
+    <div className="w-36 md:w-48 pr-4">
       <img alt="Movies Card" src={image?.medium} />
     </div>
   );
