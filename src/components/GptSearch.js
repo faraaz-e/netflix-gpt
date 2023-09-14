8bfdcc3fc8339bf850394a34b24b3cc2e9b7ecbf
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-14 20:54:23 +0530

    UI responsive

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,12 +1,21 @@
 import GptSearchBar from "./GptSearchBar";
 import GptMovieSuggestions from "./GptMovieSuggestions";
+import { BG_URL } from "../utils/constants";
 
 const GptSearch = () => {
   return (
-    <div>
-      <GptSearchBar />
-      <GptMovieSuggestions />
-    </div>
+    <>
+      <div className="fixed -z-10">
+        <img
+          className="h-screen w-screen opacity-90 object-cover"
+          src={BG_URL}
+        />
+      </div>
+      <div>
+        <GptSearchBar />
+        <GptMovieSuggestions />
+      </div>
+    </>
   );
 };
 
